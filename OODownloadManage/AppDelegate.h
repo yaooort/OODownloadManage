f8fbc2ed4ff02ff1782d37f52830fe67c82b0597
bunny <bunny@moonjoy.net>
2019-04-10 14:02:24 +0800

    Initial Commit

---
+++ OODownloadManage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OODownloadManage
+//
+//  Created by bunny on 2019/4/10.
+//  Copyright © 2019 Oort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OODownloadManage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OODownloadManage
+//
+//  Created by bunny on 2019/4/10.
+//  Copyright © 2019 Oort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
